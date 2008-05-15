6fefdd19a707b15a3e0da9420660ad9c17f6ac5a
Christoph Koehler <christoph@webempoweredchurch.org>
2008-05-15 01:45:24 +0000

    create localconf file and set pageNotFound handling to our userfunc

---
+++ wec_errorpage/ext_localconf.php
@@ -0,0 +1,6 @@
+<?php
+if (!defined ('TYPO3_MODE')) 	die ('Access denied.');
+
+$TYPO3_CONF_VARS['FE']['pageNotFound_handling'] = 'USER_FUNCTION:'.t3lib_extMgm::extPath('wec_errorpage').'class.tx_wecerrorpage_handler.php:tx_wecerrorpage_handler->pageNotFound';
+
+?>
